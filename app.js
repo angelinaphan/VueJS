97121b16909a25f1166bc666a336967b04236ae3
angelinaphan <91085907+angelinaphan@users.noreply.github.com>
2024-02-05 09:26:59 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('hello, vue')
+// console.log('hello, vue')
+
+const app = Vue.createApp({
+    // data, functions
+    // template: '<h2> I am the template </h2>'
+
+    data() {
+        return {
+            title: 'hi',
+            author: 'there',
+            age: 18
+        }
+    },
+    methods: {
+        changeTitle(abc) {
+           // this.title = 'words of randiance'
+           this.title = abc
+        }
+    }
+})
+
+app.mount('#app')
+
+// 21: 17
